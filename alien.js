4c842bafd86181913f0818e929de37e307f592dc
Rafael2023272 <145032666+Rafael2023272@users.noreply.github.com>
2024-06-10 13:58:55 +0400

    Add files via upload

---
+++ alien.js
@@ -0,0 +1,37 @@
+function setup() {
+  createCanvas(400, 400);
+  background(40);
+  fill(90, 150, 10);
+  ellipse(200, 300, 180, 280);
+  fill(20);
+  ellipse(170, 200, 50, 40);
+  ellipse(230, 200, 50, 40);
+  fill(20, 90, 50);
+  ellipse(150, 280, 40, 20);
+  ellipse(250, 280, 40, 20);
+  fill(255);
+  ellipse (170, 200, 15, 30);
+  ellipse(230, 200, 15, 30);
+  fill(70);
+  ellipse (200, 145, 10, 50);
+  fill(70);
+  ellipse (220, 150, 10, 50);
+  fill(70);
+  ellipse (180, 150, 10, 50);
+  fill(150);
+  ellipse (200, 120, 20, 20);
+  fill(200);
+  ellipse (200, 330, 90, 10);
+  fill(90);
+  ellipse (200, 400, 200, 60);
+}
+function draw() {
+}
+
+
+
+
+
+
+
+
